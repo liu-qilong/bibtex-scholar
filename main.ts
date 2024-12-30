b7f4bf19337fe1c019df01c343e7989b75c8616d
KNPOB <qilong-kirov.liu@outlook.com>
2024-12-30 21:13:34 +0800

    read & parse bibtex code block

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian'
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, MarkdownRenderer } from 'obsidian'
+import { parse_bitex } from 'src/parse'
 
 interface BibtexScholarSettings {
 	mySetting: string
@@ -10,12 +11,29 @@ const DEFAULT_SETTINGS: BibtexScholarSettings = {
 
 export default class BibtexScholar extends Plugin {
 	settings: BibtexScholarSettings
+	allbibtex = []
 
 	async onload() {
 		await this.load_settings()
 
 		// settings tab
 		this.addSettingTab(new BibtexScholarSettingTab(this.app, this))
+
+		this.registerMarkdownCodeBlockProcessor('bibtex', (source, el, ctx) => {
+			const fields = parse_bitex(source)
+
+			el.createEl('hr')
+			const table = el.createEl('table')
+      		const body = table.createEl('tbody')
+			
+			for (const key in fields) {
+				const tr = body.createEl('tr')
+				const td1 = tr.createEl('td')
+				td1.innerText = key
+				const td2 = tr.createEl('td')
+				MarkdownRenderer.render(this.app, fields[key], td2, '', this)
+			}
+		})
 	}
 
 	onunload() {
---
+++ src/parse.ts
@@ -0,0 +1,86 @@
+interface BibtexField {
+    [key: string]: string;
+}
+
+export function parse_bitex(bibtex_source: string, lower_case_type: boolean = true): BibtexField | null {
+    // match type, id, & fields
+    const entryRegex = /@([a-zA-Z]+){([^,]+),(.*)}/g
+    let match = entryRegex.exec(bibtex_source.replace(/\n/g, ''))
+
+    if (!match) {
+        console.error('No match found for the provided BibTeX source.');
+        return null;
+    }
+
+    let fields: BibtexField = {}
+    fields['type'] = match[1]
+    fields['id'] = match[2]
+    let fields_str = match[3]
+
+    console.log(bibtex_source)
+
+    // parse bibtex fields
+    let mode = 'key'
+    let store = ''
+    let max_layer = 0
+    let stack = []
+    let keys = []
+    let values = []
+
+    for (let [idx, char] of [...fields_str].entries()) {
+        if (mode === 'key') {
+            // parsing the key of a field
+            if (char === '=') {
+                keys.push(store.replace(/^[, ]+/, '').replace(/[, ]+$/, ''))
+                store = ''
+                mode = 'value'
+            } else {
+                store += char
+            }
+        } else if (mode === 'value') {
+            // parsing the value of a field
+            store += char
+
+            if (char === '{') {
+                stack.push(char)
+                max_layer += 1
+
+                // if the value has {} pairs, remove the outmost {
+                if (max_layer === 1) {
+                    store = ''
+                }
+            } else if (char === '}') {
+                stack.pop()
+                
+                // if the value has {} pairs, discard the outmost }
+                if (stack.length === 0) {
+                    store = store.slice(0, -1)
+                }
+            }
+
+            if ((max_layer > 0 && stack.length === 0) || (max_layer === 0 && (char === ',' || char === '}' || idx === fields_str.length - 1))) {
+                // when the field has {} pairs, complete parsing when the '{' stack is empty
+                // when the field has no {} layers, complete parsing when the ',' or '}' is encountered or the string ends
+                let value = store.replace(/^[ ]+/, '').replace(/[, ]+$/, '')
+
+                if (value[0] === '{' || value[-1] === '}') {
+                    value = `"${value}"`
+                }
+
+                values.push(value)
+                store = ''
+                max_layer = 0
+                mode = 'key'
+            }
+        }
+    }
+    
+    keys.map((key, idx) => { fields[key.toLowerCase()] = values[idx] })
+
+    // if lower_case_type is true, convert bib_type to lower case
+    if (lower_case_type === true) {
+        fields['type'] = fields['type'].toLowerCase()
+    }
+
+    return fields
+}
